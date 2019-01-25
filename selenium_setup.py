5701ab73d4c23de71a8eaa101465ab12153a9d02
Maxi <maximilian.lohmann.hh@gmail.com>
2019-01-25 14:34:15 +0100

    Path to the chromedriver is read from the environment variable `CHROMEDRIVER`

--- selenium_setup.py
+++ selenium_setup.py
@@ -1,12 +1,16 @@
 from selenium import webdriver
-
-chromedriver_path = './chromedriver'
+from os import environ
 
 
 def selenium_setup(fullscreen=True):
+    chromedriver_path = environ["CHROMEDRIVER"]
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--disable-infobars")
     driver = webdriver.Chrome(chromedriver_path, chrome_options=chrome_options)
     if fullscreen:
         driver.fullscreen_window()
     return driver
+
+
+if __name__ == "__main__":
+    environ["CHROMEDRIVER"] = './chromedriver'
