0d79d9b3305d6c207bb9c974ea3254574216b720
Maxi <maximilian.lohmann.hh@gmail.com>
2019-04-08 18:03:34 +0200

    Increment version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="BrowserAutomator",
-    version="1.0.8",
+    version="1.0.9",
     author="EDEKA DIGITAL",
     description="A package to simplify browser automation",
     long_description=long_description,
